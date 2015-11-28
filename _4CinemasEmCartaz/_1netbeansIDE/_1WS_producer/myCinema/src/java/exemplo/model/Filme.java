3c27fe3747656ea0d69005ac717e2c701866858e
atafs <atafs@iscte-iul.pt>
2015-11-28 11:43:37 +0000

    update

---
+++ _4CinemasEmCartaz/_1netbeansIDE/_1WS_producer/myCinema/src/java/exemplo/model/Filme.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo.model;
+
+/**
+ *
+ * @author Americo
+ */
+public class Filme {
+    
+}
